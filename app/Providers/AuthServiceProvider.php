08e66ce3795ec6bd7b68c1093e42198774976cd5
wifi user4 <login4@cec.edu.ph>
2019-02-18 09:41:27 +0800

    Update

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Laravel\Passport\Passport;
+use App\Models\Passport\Client;
+use App\Models\Passport\AuthCode;
+use App\Models\Passport\TokenModel;
+use App\Models\Passport\PersonalAccessClient;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -28,6 +32,12 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+          Passport::loadKeysFrom('/secret-keys/oauth');
         //
+
+          Passport::useClientModel(Client::class);
+    Passport::useTokenModel(TokenModel::class);
+    Passport::useAuthCodeModel(AuthCode::class);
+    Passport::usePersonalAccessClientModel(PersonalAccessClient::class);
     }
 }
